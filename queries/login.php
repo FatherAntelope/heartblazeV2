9468d70c59bc0d72d3d067c7f918a556a9864968
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2020-11-02 20:04:19 +0500

    Оптимизирован код аутентификации

--- queries/login.php
+++ queries/login.php
@@ -5,25 +5,10 @@ R::freeze(true);
 
 $user = R::findOne('person', ' login = ? ', [ $_POST['login'] ]);
 
-if ($user == NULL) {
-	die(header("HTTP/1.0 400 Bad Request"));
-}
-
-if (!password_verify($_POST['password'], $user->password)) {
-	die(header("HTTP/1.0 400 Bad Request"));
-}
-
-setcookie('userInfo', $user, time() + (86400 * 30), "/");
-setcookie('role', $user->role, time() + (86400 * 30), "/");
-
-if ($user->role == 0) {
-	header('Location: /student/lk.php');
+if($user && password_verify($_POST['password'], $user->password)) {
+    setcookie('userInfo', $user, time() + (86400 * 30), "/");
+    setcookie('role', $user->role, time() + (86400 * 30), "/");
 } else {
-	if ($user -> role == 1) {
-		header('Location: /professor/lk.php');
-	} else {
-		header('Location: /administrator/lk.php');
-	}
+    die(header("HTTP/1.0 400 Bad Request"));
 }
-
 ?>
