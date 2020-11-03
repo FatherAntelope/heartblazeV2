ab5c93c56cda21742a20e00b1afe34384bb2f12e
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2020-11-03 15:27:08 +0500

    Куки теперь хранит ID

--- queries/login.php
+++ queries/login.php
@@ -1,13 +1,11 @@
 <?php
-
 require $_SERVER['DOCUMENT_ROOT']."/db/db.php";
 R::freeze(true);
 
 $user = R::findOne('person', ' login = ? ', [ $_POST['login'] ]);
 
 if($user && password_verify($_POST['password'], $user->password)) {
-    setcookie('userInfo', $user, time() + (86400 * 30), "/");
-    setcookie('role', $user->role, time() + (86400 * 30), "/");
+    setcookie('userID', $user->id, time()+(60*60*24*30), "/");
 } else {
     die(header("HTTP/1.0 400 Bad Request"));
 }
