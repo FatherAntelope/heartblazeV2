73afa252b5d2a23402a640dbbba781b172dd35e6
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2020-11-08 20:49:04 +0500

    Выровнена таблица

--- db/db.php
+++ db/db.php
@@ -2,9 +2,9 @@
 //mail('gorbunov.vladlen2014@gmail.com', 'My Subject', "http://heartblaze.ru/?email_key=fsddfhfdjjfd");
 require $_SERVER['DOCUMENT_ROOT']."/frameworks/rb.php";
 $username = 'root';
-$password = 'root';
+$password = 'admin';
 $dbname = 'heartblaze';
-$hostname = 'localhost';
+$hostname = '192.168.0.103';
 R::setup( "mysql:host={$hostname};dbname={$dbname}", $username, $password);
 if ( !R::testConnection() )
 {
