d0c302cc2b9a3effe2adbb22031049c7d456bc35
Rail <rail1999g102r@mail.ru>
2020-11-16 16:30:06 +0500

    fix removing professor & student

--- queries/administrator/removeProfessor.php
+++ queries/administrator/removeProfessor.php
@@ -4,6 +4,8 @@ require $_SERVER['DOCUMENT_ROOT']."/db/db.php";
 
 $prof_id = $_POST['id'];
 $proffessor = R::load('professor', $prof_id);
-R::trash($proffessor);
+$person = R::load('person', $proffessor->idPerson);
+R::trash($person);
+
 
 ?>
--- queries/administrator/removeStudent.php
+++ queries/administrator/removeStudent.php
@@ -4,7 +4,7 @@ require $_SERVER['DOCUMENT_ROOT']."/db/db.php";
 
 $student = R::load('student', $_POST['id']);
 $person = R::load('person', $student->idPerson);
-R::trash($student);
 R::trash($person);
+
 
 ?>
